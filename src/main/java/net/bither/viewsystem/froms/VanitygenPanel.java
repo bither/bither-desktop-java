ec4ef8cdc94bbd1a7d608525e9f6a5b7854489db
songchenwen <sanvinsung@gmail.com>
2015-03-28 15:48:08 +0800

    edit vanity address generation view

--- src/main/java/net/bither/viewsystem/froms/VanitygenPanel.java
+++ src/main/java/net/bither/viewsystem/froms/VanitygenPanel.java
@@ -41,6 +41,7 @@ import java.awt.ComponentOrientation;
 import java.awt.Font;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,7 +50,7 @@ import java.util.Arrays;
  * Created by nn on 15/3/19.
  */
 public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelegate,
-        ListSelectionListener {
+        ListSelectionListener, ActionListener {
 
     private DialogPassword.PasswordGetter passwordGetter;
     private JTextField textField;
@@ -61,6 +62,8 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
     private JLabel lblOne;
 
     private JLabel lblSelectDevice;
+    private JCheckBox cbxCPU;
+    private JCheckBox cbxGPU;
     private JLabel lblLoadingDevices;
     private JTable tbDevices;
     private JScrollPane sp;
@@ -75,8 +78,10 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
     private String[] privateKeys;
 
     private PeriodFormatter remainingTimeFormatter;
-
     private JPanel panel;
+
+    private Thread refreshingUIThread;
+    private Thread computingThread;
 
     public VanitygenPanel() {
         super(MessageKey.vanity_address, AwesomeIcon.VIMEO_SQUARE, true);
@@ -94,7 +99,7 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
             public void actionPerformed(ActionEvent e) {
                 if (isInCalculatingView) {
                     generateAddress();
-                } else if (selectedDevice != null) {
+                } else {
                     showCalculate();
                 }
             }
@@ -112,7 +117,8 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
         caseInsensitiveBox = new JCheckBox(LocaliserUtils.getString("vanity_case_insensitive"));
 
         pb = new JProgressBar();
-        pb.setValue(50);
+        pb.setValue(0);
+        pb.setMaximum(1000);
         pb.setVisible(false);
 
         textField = TextBoxes.newEnterAddress(new DocumentListener() {
@@ -136,22 +142,27 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
         lblSpeed = Labels.newValueLabel("");
         lblTimeRemain = Labels.newValueLabel("");
 
+
         lblSelectDevice = Labels.newValueLabel(LocaliserUtils.getString
                 ("vanity_select_computation_device"));
         Font font = lblSelectDevice.getFont();
         lblSelectDevice.setFont(font.deriveFont(14.0f));
         lblLoadingDevices = Labels.newSpinner(Themes.currentTheme.fadedText(), BitherUI
                 .NORMAL_PLUS_ICON_SIZE);
+        cbxCPU = new JCheckBox("CPU");
+        cbxCPU.addActionListener(this);
+        cbxGPU = new JCheckBox("GPU (OpenCL)");
+        cbxGPU.addActionListener(this);
         tbDevices = new JTable(selectDeviceTableModel);
         tbDevices.getColumnModel().getColumn(0).setResizable(true);
         tbDevices.getColumnModel().getColumn(1).setResizable(true);
 
-        tbDevices.getColumnModel().getColumn(0).setMinWidth(1);
-        tbDevices.getColumnModel().getColumn(0).setPreferredWidth(Integer.MAX_VALUE);
+        tbDevices.getColumnModel().getColumn(1).setMinWidth(1);
+        tbDevices.getColumnModel().getColumn(1).setPreferredWidth(Integer.MAX_VALUE);
 
-        tbDevices.getColumnModel().getColumn(1).setMinWidth(20);
-        tbDevices.getColumnModel().getColumn(1).setPreferredWidth(20);
-        tbDevices.getColumnModel().getColumn(1).setCellRenderer(new SelectAddressImage());
+        tbDevices.getColumnModel().getColumn(0).setMinWidth(20);
+        tbDevices.getColumnModel().getColumn(0).setPreferredWidth(20);
+        tbDevices.getColumnModel().getColumn(0).setCellRenderer(new SelectAddressImage());
         tbDevices.setOpaque(true);
         tbDevices.setAutoCreateColumnsFromModel(true);
         tbDevices.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
@@ -172,11 +183,15 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
 
     private void showSelectDevice() {
         isInCalculatingView = false;
-        panel.add(lblSelectDevice, "align center, cell 1 2, wrap");
-        panel.add(lblLoadingDevices, "align center, cell 1 3, wrap");
-        panel.add(sp, "cell 0 3 3 7, grow");
+        panel.add(lblSelectDevice, "align center, cell 0 0 3 1, wrap");
+        panel.add(lblLoadingDevices, "align center, cell 0 1 3 1, wrap");
+        panel.add(cbxCPU, "align left, cell 0 2, wrap");
+        panel.add(cbxGPU, "align left, cell 0 3, wrap");
+        panel.add(sp, "cell 0 4 3 8, grow");
         lblLoadingDevices.setVisible(false);
         sp.setVisible(false);
+        cbxCPU.setVisible(false);
+        cbxGPU.setVisible(false);
         refreshDevices();
     }
 
@@ -186,10 +201,14 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
         lblSelectDevice.setVisible(false);
         lblLoadingDevices.setVisible(false);
         sp.setVisible(false);
+        cbxCPU.setVisible(false);
+        cbxGPU.setVisible(false);
 
         panel.remove(lblSelectDevice);
         panel.remove(lblLoadingDevices);
         panel.remove(sp);
+        panel.remove(cbxCPU);
+        panel.remove(cbxGPU);
 
         panel.add(lblOne, "align right,cell 0 2,wrap");
         panel.add(textField, "align center,cell 1 2,grow");
@@ -214,11 +233,11 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
             textField.setEnabled(false);
             caseInsensitiveBox.setEnabled(false);
             setOkEnabled(false);
-
-            new Thread(new Runnable() {
+            computingThread = new Thread() {
                 @Override
                 public void run() {
-
+                    // TODO use shouldUseOpenCL to determine which process to call
+                    shouldUseOpenCL();
                     NativeUtil.generateAddress(input);
                     privateKeys = NativeUtil.getPrivateKey();
                     final SecureCharSequence password = passwordGetter.getPassword();
@@ -233,8 +252,9 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
                         }
                     });
                 }
-            }).start();
-            new Thread() {
+            };
+            computingThread.start();
+            refreshingUIThread = new Thread() {
                 @Override
                 public void run() {
                     while (privateKeys == null && !isInterrupted()) {
@@ -250,7 +270,8 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
                             SwingUtilities.invokeLater(new Runnable() {
                                 @Override
                                 public void run() {
-                                    pb.setValue((int) (progress * 100));
+                                    pb.setValue((int) (pb.getMinimum() + progress * (pb
+                                            .getMaximum() - pb.getMinimum())));
                                     lblDifficulty.setText(String.format(LocaliserUtils.getString
                                             ("vanity_difficulty"), difficulty));
                                     lblGenerated.setText(String.format(LocaliserUtils.getString
@@ -270,7 +291,8 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
                         }
                     }
                 }
-            }.start();
+            };
+            refreshingUIThread.start();
         }
     }
 
@@ -288,10 +310,10 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
         @Override
         public Object getValueAt(int rowIndex, int columnIndex) {
             switch (columnIndex) {
-                case 0:
+                case 1:
                     return devices.get(rowIndex).getPlatformName() + " : " + devices.get
                             (rowIndex).getDeviceName();
-                case 1:
+                case 0:
                     return devices.get(rowIndex).equals(selectedDevice);
             }
             return null;
@@ -306,6 +328,8 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
     private void refreshDevices() {
         lblLoadingDevices.setVisible(true);
         sp.setVisible(false);
+        cbxGPU.setVisible(false);
+        cbxCPU.setVisible(false);
         new Thread() {
             @Override
             public void run() {
@@ -321,15 +345,21 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
                 SwingUtilities.invokeLater(new Runnable() {
                     @Override
                     public void run() {
-                        if (devices.size() == 0) {
-                            //TODO no device can't use oclvanitygen
+                        if (devices.size() <= 1) {
+                            showCalculate();
                             return;
                         }
-                        selectedDevice = devices.get(0);
-                        if (devices.size() == 1) {
-                            showCalculate();
+                        for (OpenCLDevice d : devices) {
+                            if (d.isGPU()) {
+                                selectedDevice = d;
+                                break;
+                            }
                         }
+                        cbxCPU.setSelected(selectedDevice == null);
+                        cbxGPU.setSelected(selectedDevice != null);
                         sp.setVisible(true);
+                        cbxGPU.setVisible(true);
+                        cbxCPU.setVisible(true);
                         lblLoadingDevices.setVisible(false);
                         selectDeviceTableModel.fireTableDataChanged();
                     }
@@ -338,6 +368,24 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
         }.start();
     }
 
+    private boolean shouldUseOpenCL() {
+        return selectedDevice != null;
+    }
+
+    @Override
+    public void closePanel() {
+        super.closePanel();
+        if (refreshingUIThread != null && refreshingUIThread.isAlive() && !refreshingUIThread
+                .isInterrupted()) {
+            refreshingUIThread.interrupt();
+        }
+        if (computingThread != null && computingThread.isAlive() && !computingThread
+                .isInterrupted()) {
+            //TODO stop vanity address generation
+            computingThread.interrupt();
+        }
+    }
+    
     private String secondsToString(long seconds) {
         return remainingTimeFormatter.print(new Period(seconds * 1000));
     }
@@ -356,7 +404,40 @@ public class VanitygenPanel extends WizardPanel implements IPasswordGetterDelega
                     break;
                 }
             }
+            if (selectedDevice != null) {
+                cbxCPU.setSelected(false);
+                cbxGPU.setSelected(true);
+            }
             selectDeviceTableModel.fireTableDataChanged();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == cbxCPU) {
+            if (cbxCPU.isSelected()) {
+                selectedDevice = null;
+                cbxGPU.setSelected(false);
+                selectDeviceTableModel.fireTableDataChanged();
+            } else {
+                cbxCPU.setSelected(true);
+            }
+            return;
+        } else if (e.getSource() == cbxGPU) {
+            if (cbxGPU.isSelected()) {
+                if(devices.size() < 1){
+                    cbxGPU.setSelected(false);
+                    return;
+                }
+                if (selectedDevice == null) {
+                    selectedDevice = devices.get(0);
+                }
+                cbxCPU.setSelected(false);
+                selectDeviceTableModel.fireTableDataChanged();
+            } else {
+                cbxGPU.setSelected(true);
+            }
+            return;
         }
     }
 
