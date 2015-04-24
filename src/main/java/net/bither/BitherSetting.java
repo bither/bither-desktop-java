fc4d12c3326f4ca39b0e5f039362c4c87aaac4e4
jjz <woaf1003@gmail.com>
2015-04-24 11:38:26 +0800

    1.3.2->1.3.4

--- src/main/java/net/bither/BitherSetting.java
+++ src/main/java/net/bither/BitherSetting.java
@@ -8,8 +8,8 @@ import java.util.Date;
 
 public class BitherSetting {
 
-    public static final String VERSION = "1.3.2";
-    public static final int VERSION_CODE = 132;
+    public static final String VERSION = "1.3.4";
+    public static final int VERSION_CODE = 134;
 
     public static final int NOT_RELEVANT_PERCENTAGE_COMPLETE = -1;
 
