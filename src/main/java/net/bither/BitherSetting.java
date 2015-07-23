dbd48139319a95e01634fae8d2fbf9594341b8c9
songchenwen <sanvinsung@gmail.com>
2015-07-23 16:36:59 +0800

    Bump Version

--- src/main/java/net/bither/BitherSetting.java
+++ src/main/java/net/bither/BitherSetting.java
@@ -26,7 +26,7 @@ import java.util.Date;
 
 public class BitherSetting {
 
-    public static final String VERSION = "1.3.7";
+    public static final String VERSION = "1.3.7.1";
     public static final int VERSION_CODE = 137;
 
     public static final int NOT_RELEVANT_PERCENTAGE_COMPLETE = -1;
